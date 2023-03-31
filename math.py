2aaa9dbd9ed646d32393e18e41500226778f3860
ChristineAnne-Adhiambo <annechristineadhiambo@gmail.com>
2023-03-31 15:45:31 +0300

    Python functions

---
+++ math.py
@@ -0,0 +1,25 @@
+#Addition
+
+def add(a,b):
+  answer = a + b
+  return answer
+
+#Subtraction
+def subtract(x,y):
+   result = x + y
+   return result
+
+#Multiplication
+def multiply(x,y):
+   result = x * y
+   return result
+   
+#Division
+   def divide(n,c):
+    division = n/c
+   return division
+
+   #Modulus
+   def remainder(e,f):
+    modulus = e%f
+   return modulus
